0b7cb29323e0c75f07cb02fe88c6ccbe96947180
mr11011 <m.elrouby@nu.edu.eg>
2024-08-18 00:27:59 +0300

    add login page design

---
+++ lib/modules/loginPages/LoginPage.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class logInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Sign In ",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "WELCOME BACK!!  😁🪥 ",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Email",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Enter Your Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Password",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Enter Your Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15))),
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Forgot Password??",
+                  style: TextStyle(color: Colors.deepPurple),
+                )),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account yet ?!"),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(color: Colors.deepPurple),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
